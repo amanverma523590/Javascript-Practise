3f7b901c9fad50880c3d8da7525771e160b735e0
Aman Verma <aman97104@gmail.com>
2025-12-19 21:10:26 +0545

    object-project

--- object/script.js
+++ object/script.js
@@ -1,17 +1,17 @@
-let obj1 = {
-  name : "Aman",
-  age : 26,
-  greet : function(){
-    console.log(`hii ${obj1.name}`);
-  }
-}
+// let obj1 = {
+//   name : "Aman",
+//   age : 26,
+//   greet : function(){
+//     console.log(`hii ${obj1.name}`);
+//   }
+// }
 
-let obj2 = {
-  name : "sanam",
-  age : 30,
-}
- obj2.greet = obj1.greet;
- console.log(obj2.greet());
+// let obj2 = {
+//   name : "sanam",
+//   age : 30,
+// }
+//  obj2.greet = obj1.greet;
+//  console.log(obj2.greet());
 
 // let ans = obj1.greet();
 // console.log(ans);
@@ -31,3 +31,18 @@ let obj2 = {
 
 // ### 2️⃣ Makes code clean and maintainable  
 // By using `this`, we avoid hardcoding object names, which makes the code easier to update and maintain.
+
+
+function GetClick(){
+  document.getElementById('detail-container').style.display = "block";
+  fetch("product.json")
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(product){
+    document.getElementById("labelName").innerHTML = product.name;
+    document.getElementById("labelPrice").innerHTML = product.price;
+    document.getElementById('productImg').src = product.photo;
+    document.getElementById('labelStock').innerHTML = product.stock==true ? "Availbale":"Not Available";
+  })
+}
