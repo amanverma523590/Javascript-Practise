78f9560a453d3586bcb32f2c485dc192f0d47781
Aman Verma <aman97104@gmail.com>
2025-12-22 10:27:38 +0545

    event

---
+++ Events/script.js
@@ -0,0 +1,26 @@
+// function handleClick(){
+//     var heading = document.getElementById("heading");
+//     if(heading.textContent === "Hello"){
+//         heading.textContent = "bye";
+//     }else{
+//         heading.textContent = "Hello"
+//     }
+// }
+
+// var heading = document.getElementById("heading");
+// heading.addEventListener(('click'),()=>{
+//     heading.style.backgroundColor = "Pink"
+// })
+
+let parent = document.getElementById("parent");
+
+console.log(parent.children);
+
+for(let child of parent.children){
+    console.log(child);
+    child.addEventListener(('click'),()=>{
+        child.textContent = "I am Clicked"
+    })
+
+    
+}
