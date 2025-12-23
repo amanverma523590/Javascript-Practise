c8baf082e5613fa28ff5f37e630b6f3d8cfa2123
Aman Verma <aman97104@gmail.com>
2025-12-23 21:28:17 +0545

    event

--- Events/script.js
+++ Events/script.js
@@ -24,4 +24,53 @@
 
     
 // }
+// Event argument
 
+// function DataOperation(obj,e,list,ob){
+//     console.log(`Id=${obj.id} 
+//                 ClientX = ${e.clientX}    
+//                 ClientY = ${e.clientY}
+//                 `);
+//     console.log(list);
+//     console.log(ob);
+// }
+
+
+// function BodyLoad(){
+//    let btnInsert =  document.getElementById("btnInsert");
+//    btnInsert.addEventListener(("click"),(e)=>{
+//     console.log(`
+//             ButtonId = ${e.target.id}
+//             ButtonValue = ${e.target.value}
+//             ClientX = ${e.clientX};
+//             ControlKey = ${e.ctrlKey}
+//         `);
+//    })
+// }
+
+// function NavClick(){
+//     alert("NavClicked")
+// }
+
+// function SearchClick(e){
+//     alert("Search Clicked")
+//     e.stopPropagation();
+// }
+
+// function handleSubmit(e){
+//     e.preventDefault();
+//     alert(`form submitted`)
+// }
+
+console.log('execution start');
+fetch(`https://fakestoreapi.com/products/categories`)
+ .then(async res => await res.json())
+ .then(data => {
+    data.map(async item =>{
+        console.log(await item);
+    })
+ })
+ setTimeout(function (){
+    console.log(`i am fetching from api`);
+ },0);
+ console.log('excution ended');
