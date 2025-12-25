778e42ca94191fb08fe45b9878297d923fdcbcd4
Aman Verma <aman97104@gmail.com>
2025-12-25 09:55:20 +0545

    bgcr changer

--- Project-2/script.js
+++ Project-2/script.js
@@ -1,1 +1,8 @@
-alert(`connected`)
+let container = document.getElementById("container");
+
+container.addEventListener("dblclick",(e)=>{
+    // console.log(e.target); // child kaun sa click hua
+    const child = e.target;
+    const body = document.querySelector('body');
+    body.style.backgroundColor = child.id; //id must be color name here
+})
