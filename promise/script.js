0c386487eb4c200b6d2b557ce2176ea55e50cd76
Aman Verma <aman97104@gmail.com>
2025-12-28 10:35:29 +0545

    solve

--- promise/script.js
+++ promise/script.js
@@ -1,29 +1,107 @@
-// console.log(`first`);
-
-// const p1 = fetch(`https://api.github.com/users`);
-
-// const p2 = p1.then((response)=>{
+// fetch(`https://api.github.com/users`)
+//   .then((response) => {
+//     if(!response.ok){
+//       throw new Error("Data is not Present")
+//     }
 //     return response.json();
+//   })
+//   .then((data) => {
+//     const parent = document.getElementById("first");
+
+//     for (let i=0; i<data.length; i++) {
+//       const img = document.createElement("img");
+//       img.src = data[i].avatar_url;
+//       img.style.height = "300px";
+//       img.style.width = "300px";
+//       parent.appendChild(img);
+//     }
+
+//   })
+// .catch((error) => {
+//     document.getElementById("first").textContent = error.message;
+//   });
+
+// creating our own promises
+
+// let p1 = new Promise((resolve,reject)=>{
+//   reject("wrong");
 // })
-// p2.then((response)=>{
-//     return response;
+// p1.then((response)=>{   // resolve than wala hancle kar lega
+//   console.log(response);
+// }).catch((error)=>{   //reject cath wala handle kar lega
+//   console.log(error);
 // })
-// console.log(p2);
 
+// #Soling our callback hell
 
-// console.log(`last`);
+//zomato
 
-// instead of doing this we do prommise chaining
+const details = {
+  uname: "Aman",
+  location: "Bydrahlli",
+  hotel: "anjana nagar",
+  food: ["Pizza", "Biryani", "Coke"],
+  price: 360,
+};
 
-console.log(`first`);
+function placeOrder(details) {
+  console.log(`Payment Pending of Rs.${details.price}`);
 
-fetch(`https://api.github.com/users`)
-.then((response)=>{
-    return response.json();
-})
-.then((data)=>{
-    console.log(data);
-})
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`Payment successful`);
+      details.status = true;
+      resolve(details);
+    }, 3000);
+  });
+}
 
+function prepareOrder(details) {
+  console.log(`${details.uname}Your order is started to prepare`);
 
-console.log(`last`);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`Order prepared`);
+      details.status = true;
+      resolve(details);
+    }, 3000);
+    
+  });
+}
+
+function deliveryBoy(details) {
+  console.log(`Delivery boy is on way to location`);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`Delivery boy has reached to hotel`);
+      details.status = true;
+      resolve(details);
+    }, 3000);
+    
+  });
+}
+
+function delivery() {
+  console.log(`ur order is out of delivery`);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`ur item has been delivered`);
+      details.status = true;
+      resolve(details);
+    }, 3000);
+    
+  });
+}
+
+placeOrder(details)
+  .then((details) => prepareOrder(details))
+  .then((details) => deliveryBoy(details))
+  .then((details) => delivery(details))
+  .then((details)=>{
+    console.log(details);
+  })
+  .catch((error)=>{
+    console.log("Error :", error.message);
+  })
