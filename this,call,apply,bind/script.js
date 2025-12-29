9850c39fd2182ed342c0f04d0851b1a587159a90
Aman Verma <aman97104@gmail.com>
2025-12-29 18:36:49 +0545

    use strict

---
+++ this,call,apply,bind/script.js
@@ -0,0 +1,14 @@
+// 'use strict'
+
+// var a = 10;
+//  const b = 20;
+
+// console.log(a,b);
+
+// function greet(name,name1){
+//     console.log(name,name1);
+// }
+// greet('aman','rohit');
+
+// #use strict allow u to write good code 
+
