d9b5a6e07f4c3d9099dcd805f66b2bf0f1ea4823
Aman Verma <aman97104@gmail.com>
2025-12-30 12:24:29 +0545

    data type

--- practise/script.js
+++ practise/script.js
@@ -3,6 +3,25 @@
 
 // console.log(a);
 
+// scope
 
+// let age = 20;
 
-//
+// function say(){
+//     console.log(age);
+// }
+// say();
+
+// functional scope
+
+// if(true){
+//     var a = 10;
+// }
+
+// function hi(){
+//     var b = 20;
+// }
+// console.log(b);
+
+let a = 5623475875n;
+console.log(a);
