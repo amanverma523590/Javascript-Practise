45327982bbcdf78491d7cb5a8a8370aa7c63224b
Aman Verma <aman97104@gmail.com>
2025-12-30 17:29:58 +0545

    react

--- practise/script.js
+++ practise/script.js
@@ -23,5 +23,28 @@
 // }
 // console.log(b);
 
-let a = 5623475875n;
-console.log(a);
+// let a = 5623475875n;
+// console.log(a);
+
+
+const root = document.getElementById('root')
+
+function createElement(tag,attribute,children){
+    
+}
+
+
+const element = document.createElement('h1');
+element.innerText = "sup coders";
+element.className = "heading1"
+root.append(element);
+
+
+
+
+
+
+
+
+
+
