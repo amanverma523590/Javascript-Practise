19cc2793a8052d8281f42bbf250cd6106dda16c0
Aman Verma <aman97104@gmail.com>
2026-01-13 20:47:34 +0545

    changed

---
+++ practise/script.js
@@ -0,0 +1,26 @@
+
+
+// let outervariable = "I am Outside";
+
+// const shadowingFunction = ()=>{
+//     let outervariable = "But i am inside";
+
+//     console.log(outervariable);
+// }
+
+// shadowingFunction();
+
+// let x = 20;
+
+// function a(){
+// const x = 10;
+
+// console.log(x);
+// }
+
+// a();
+
+let details = {
+    name : "aman",
+    age : 19
+}
