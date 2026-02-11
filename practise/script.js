a2f662356511f0842d1577d2484e7772e2b70b6b
Aman Verma <aman97104@gmail.com>
2026-02-11 16:41:59 +0530

    string & date

--- practise/script.js
+++ practise/script.js
@@ -1,19 +1,62 @@
 
 
-// console.log(Math.ceil(3.5))
-// console.log(Math.floor(3.6))
-// console.log(Math.log10(20))
-// console.log(Math.min(5,2,9,3,1,7))
-// console.log(Math.max(5,2,9,3,1,7))
-// console.log(Math.sqrt(9))
+// const str1 = "Aman";
+// const str2 = 'Sanam';
+// const date = 18;
+// const str3 = `Strike is coming on ${date}th jan`;
 
-// console.log(Math.floor(Math.random()*6)+1)
+// console.log(str3)
 
-// console.log(Math.floor(Math.random()*8)+1)
+// const str1 = "Aman Verma";
 
-// console.log(Math.floor(Math.random()*11)+15)
+// // console.log(str1.length);
+// // console.log(str1[2].toUpperCase());
+// // console.log(str1)
+// let a = str1.toUpperCase()
+// console.log(a)
+// console.log(str1)
 
-//15-25
+// const msg = `Aman Verma`;
 
-// console.log(Math.floor(Math.random()*(max-min+1))+min);
-console.log(Math.floor(Math.random()*(25-15+1))+15);
+// console.log(msg.slice(-3,-1))
+// console.log(msg.substring(1,6))
+
+// const a = "apple";
+// const b = "bananna";
+// const c = a+" "+b;
+
+// console.log(c)
+
+// console.log(10+"aman");
+// console.log("10"+"aman");
+// console.log("10"+5+2);  // irght me hai to no addition
+// console.log(10+5+"2");  //left me hai to pahele add then concat
+
+// const str1="aman is reading is coding"
+// console.log(str1.replaceAll('is','was'));
+
+// const str1 = "         aman is coding       Verma    ";
+// console.log(str1.trim())
+
+
+// const str1 = "aman,sanam,sahil,anjali";
+// console.log(str1.split(","));  //return new array 
+
+
+// --------------------------------------------------------------
+
+//Date 
+
+// const Now = new Date();
+// console.log(Now)
+// console.log(Now.toLocaleDateString())
+// console.log(Now.getDay())
+// console.log(Now.getMonth())
+// console.log(Now.getFullYear())
+// console.log(Now.getHours())
+// console.log(Now.getMinutes())
+// console.log(Now.getSeconds())
+
+const now = Date.now();
+const date = new Date(0);
+console.log(date.toString())
