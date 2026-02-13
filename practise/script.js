819f5827343c8c51b368cb7145bca4cd858f1694
Aman Verma <aman97104@gmail.com>
2026-02-13 16:47:42 +0530

    object revise

--- practise/script.js
+++ practise/script.js
@@ -80,11 +80,98 @@
 
 
 //slice
-const arr = [1,2,3,4,5,6];
-const newarr = arr.slice(1,4); //2,3,4
-console.log(newarr); // return new arr
+// const arr = [1,2,3,4,5,6];
+// const newarr = arr.slice(1,4); //2,3,4
+// console.log(newarr); // return new arr
 
-//splice 
-const arr1 = [1,2,3,4,5,6];
-let naya = arr1.splice(1,4,'aman',19);
-console.log(arr1)  // original array se gayab
+// //splice 
+// const arr1 = [1,2,3,4,5,6];
+// let naya = arr1.splice(1,4,'aman',19);
+// console.log(arr1)  // original array se gayab
+
+// let arr = [10,20,30];
+// let arr2 = ['aman','sahil','sanam'];
+
+// let newarr = arr.concat(arr2);
+// console.log(newarr);
+
+// let arr = [10,20,30]
+// let arr1 = ['aman','naman']
+// let arr2 = [true,false]
+
+// let arr4 = [...arr,...arr1,...arr2];
+// console.log(arr4)
+
+// const names = ['aman','sanma','rohit','mohit','sahil']
+// console.log(names.sort());
+// console.log(names.reverse());
+// console.log(names.join('+'))
+// console.log(typeof names.toString());
+
+// const a = [101,70,20,50,30,7];
+// console.log(a.sort((a,b)=>a-b))
+
+// const arr = [1,2,3,[4,5,[-1,-2,-3],6],7,8];
+// const a = arr.flat(Infinity);
+// console.log(a)
+
+// const user = {
+//     name : "Aman",
+//     age : 26,
+//     city : {
+//         pincode : 560091,
+//         area : 'bayadrahalli'
+//     },
+//     "home address": 'nepalgunj'
+// }
+
+// for(let [keys,value] of Object.entries(user) ){
+//     console.log(keys,value)
+// }
+
+// const {name,age,city} = user;
+// console.log(name,age,city.pincode)
+
+// const arr  = [10,20,30,40] 
+// const [first,second] = arr;
+// console.log(first,second)
+
+// const name = user.name;
+// console.log(name)
+
+// for(let keys in user){
+//     console.log(keys, user[keys])
+// }
+
+// console.log(Object.values(user))
+// console.log(Object.entries(user))
+
+// const user2 = user; // copy by refernce so change in user 2 reflect user1 becuase same address point
+// user.name = "sanma"
+// console.log(user2)
+
+// console.log(user["name"])
+// console.log(user['city']['area'])
+// user.lastname = "verma"
+// delete user.age;
+// console.log(user.name,user.age,user.city.pincode,user.lastname)
+// console.log(user)
+
+let user = {
+    name :'aman',
+    age :26,
+    address : {
+        city:'bangalore',
+        pin : 560091
+    },
+    greeting : function(){
+     console.log(`my name is ${this.name}`);
+     return 20;
+    } 
+}
+let user2 = {
+    name : 'sanma',
+    age : 35
+}
+user2.greeting = user.greeting;
+user2.greeting()
